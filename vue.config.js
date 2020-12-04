cae7834447dbf0ff62e1598db06f7244b4cc3cab
Andres Kalev <andres.kalev@fleetcomplete.eu>
2020-12-04 12:26:41 +0200

    Fixed path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/docs' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/vuepwa2' : '/',
     pwa: {
         workboxPluginMode: 'InjectManifest',
         workboxOptions: {
